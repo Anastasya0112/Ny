6a47b95bb7fe2a854ba19ba8990aa967c37b7e2a
Anastasya0112 <karasik1993@yandex.ru>
2018-11-11 03:27:24 +0400

    2

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import pickle
 
 root = Tk()
 #назване важно
@@ -14,7 +15,7 @@ def registation():
     registr_password1 = Entry()
     text_password2 = Label(text="Введите повторно пароль")
     registr_password2 = Entry(show="*")
-    button_registr = Button(text="Зарегистрироваться")
+    button_registr = Button(text="Зарегистрироваться", command=lambda: save())
     text.pack()
     text_login.pack()
     registr_login.pack()
@@ -24,6 +25,41 @@ def registation():
     registr_password2.pack()
     button_registr.pack()
 
+    def save():
+        login_pass_save = {}#создаем словарь
+        login_pass_save[registr_login.get()]=registr_password1.get()
+        doc = open("login.txt", "wb")
+        pickle.dump(login_pass_save, doc)
+        doc.close()
+        login()
+
+def login():
+    text_log = Label(text="Теперь можно работать в системе")
+    text_enter_login = Label(text="Введите ваш логин")
+    enter_login = Entry()
+    text_enter_password = Label(text="Введите ваш пароль")
+    enter_password = Entry(show="*")
+    button_enter = Button(text="Войти", command = lambda: password_true())
+    text_log.pack()
+    text_enter_login.pack()
+    enter_login.pack()
+    text_enter_password.pack()
+    enter_password.pack()
+    button_enter.pack()
+
+    def password_true():
+        doc = open("login.txt", "rb")
+        file_doc = pickle.load(doc)
+        doc.close()
+        if enter_login.get() in file_doc:
+            if enter_password.get() == file_doc[enter_login.get()]:
+                messagebox.showinfo("Вход выполнен успешно!")
+            else:
+                messagebox.showerror("Ощибка")
+        else:
+            messagebox.showerror("Ошибка")
+
 registation()
+
 
 root.mainloop()
